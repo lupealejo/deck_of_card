acdb28b34140db5069f0ba313d7ec00ae8ca288b
lupealejo <galejo@csumb.edu>
2017-05-15 22:50:28 -0700

    Update Assign3.java

--- Assign3.java
+++ Assign3.java
@@ -112,3 +112,68 @@ class Card
 	
 	
 }
+
+
+
+
+
+/*========================================
+Lupe's Part:
+
+//This method will remove and return the top card in the array
+public Card playCard()
+{      
+   Card topCard =  myCards[numCards - 1];
+   myCards[numCards - 1] = null; 
+   numCards-- ;
+   return topCard;
+}
+
+//Returns the string built up by the Stringizer
+ public String toString()
+   {
+      int i;
+      String cardInfo = "Hand = ( ";
+
+      for (i = 0; i < numCards; i++)
+      {
+         cardInfo += myCards[i].toString();
+         if (i < numCards - 1)
+            cardInfo += ", ";
+      }
+      cardInfo +=" )";
+      return cardInfo;
+   }
+
+static private void allocateMasterPack()
+{
+   int x, y;
+   Card.Suit cardSuit;
+   char cardValue;
+
+   // allocate 
+   masterPack = new Card[52];
+   for (y = 0; y < 52; y++)
+      masterPack[y] = new Card();
+
+   //loop for the suits 
+   //set values for suit
+   for (y = 0; y < 4; y++)
+   {
+      cardSuit = Card.Suit.values()[y];
+      masterPack[13*y].set('A', cardSuit);
+
+      for (cardValue='2', x = 1; cardValue<='9'; cardValue++, x++)
+
+         masterPack[13*y + x].set(cardValue, cardSuit);
+      masterPack[13*y+9].set('T', cardSuit);
+      masterPack[13*y+10].set('J', cardSuit);
+      masterPack[13*y+11].set('Q', cardSuit);
+      masterPack[13*y+12].set('K', cardSuit);
+   }
+}
+
+
+
+====================================== */
+
